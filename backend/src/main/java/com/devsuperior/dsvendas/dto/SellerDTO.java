d20c4d10ca2e5d7c1c0911081f0e24faa5271507
Marcio Galante <marcio.galante@gmail.com>
2021-11-07 12:58:20 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDTO.java
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDTO.java
@@ -1,4 +1,4 @@
-package com.devsuperior.dsvendas;
+package com.devsuperior.dsvendas.dto;
 
 import java.io.Serializable;
 
--- backend/src/main/java/com/devsuperior/dsvendas/service/SellerService.java
+++ backend/src/main/java/com/devsuperior/dsvendas/service/SellerService.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.devsuperior.dsvendas.SellerDTO;
+import com.devsuperior.dsvendas.dto.SellerDTO;
 import com.devsuperior.dsvendas.entities.Seller;
 import com.devsuperior.dsvendas.repositories.SellerRepository;
 
